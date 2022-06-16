d50fb519fa9ad41cea515da197bc4735ca1b51e0
Amanda Klenk <amandamklenk3@gmail.com>
2022-06-16 19:59:43 -0400

    wrote tests for Engineer's methodology and passing code in Engineer.js

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -8,3 +8,33 @@ test("creates an engineer object", () => {
     expect(engineer.email).toEqual(expect.any(String));
     expect(engineer.github).toEqual(expect.any(String));
 });
+
+test("gets engineer's name and sets it as object's name", () => {
+    const engineer = new Engineer("Dave");
+
+    expect(engineer.getName()).toHaveProperty("name");
+});
+
+test("get's engineer's id and sets it as object's id", () => {
+    const engineer = new Engineer("Dave");
+
+    expect(engineer.getId()).toHaveProperty("id");
+})
+
+test("get's engineer's email and sets it as object's email", () => {
+    const engineer = new Engineer("Dave");
+    
+    expect(engineer.getEmail()).toHaveProperty("email");
+})
+
+test("get's engineer's github username and sets it as object's github", () => {
+    const engineer = new Engineer("Dave");
+    
+    expect(engineer.getGithub()).toHaveProperty("github");
+})
+
+test("get's engineer's role and sets it as object's role", () => {
+    const engineer = new Engineer("Dave");
+    
+    expect(engineer.getRole()).toHaveProperty("role");
+})
