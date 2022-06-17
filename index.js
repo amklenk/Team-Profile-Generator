7d4b3c98e3dcbcaa71c88e348f53def21c6a16b7
Amanda Klenk <amandamklenk3@gmail.com>
2022-06-17 15:14:19 -0400

    inquirer for manager set up and running correctly, pushing into array correctly

--- index.js
+++ index.js
@@ -4,219 +4,224 @@ const fs = require("fs");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const generateHTML = require("./src/page-template")
+const generateHTML = require("./src/page-template");
+
+const managerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the team manager's name?",
+    validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter the team manager's name!");
+          return false;
+        }
+ }
+},
+{
+  type: "input",
+  name: "id",
+  message: "What is the team manager's id?",
+  validate: idInput => {
+      if (idInput) {
+        return true;
+      } else {
+        console.log("Please enter the team manager's id number!");
+        return false;
+      }
+}
+},
+{
+  type: "input",
+  name: "email",
+  message: "What is the team manager's email?",
+  validate: emailInput => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log("Please enter the team manager's email address!");
+        return false;
+      }
+}
+},
+{
+  type: "input",
+  name: "office",
+  message: "What is the team manager's office number?",
+  validate: idInput => {
+      if (idInput) {
+        return true;
+      } else {
+        console.log("Please enter the team manager's office number!");
+        return false;
+      }
+}
+}];
 
 function buildTeam() {
 this.team = [];
+this.manager;
 }
 
 buildTeam.prototype.promptManager = function (){
-  inquirer.prompt (
-    {
-      type: "input",
-      name: "name",
-      message: "What is the team manager's name?",
-      validate: nameInput => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log("Please enter the team manager's name!");
-            return false;
-          }
-   }
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "What is the team manager's id?",
-    validate: idInput => {
-        if (idInput) {
-          return true;
-        } else {
-          console.log("Please enter the team manager's id number!");
-          return false;
-        }
-  }
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What is the team manager's email?",
-    validate: emailInput => {
-        if (emailInput) {
-          return true;
-        } else {
-          console.log("Please enter the team manager's email address!");
-          return false;
-        }
-  }
-  },
-  {
-    type: "input",
-    name: "office",
-    message: "What is the team manager's office number?",
-    validate: idInput => {
-        if (idInput) {
-          return true;
-        } else {
-          console.log("Please enter the team manager's office number!");
-          return false;
-        }
-  }
-  }
+  inquirer.prompt (managerQuestions)
   .then(({name, id, email, office}) => {
-    this.team.push(new Manager(name, id, email, office));
+    this.manager = new Manager(name, id, email, office);
+    this.team.push(this.manager);
+    console.log(this.team);
     this.promptTeamMember();
   })
-  )
 };
 
 buildTeam.prototype.promptTeamMember = function () {
-inquirer.prompt(
-  {
-    type: "list",
-    name: "member",
-    message: "Which type of team member would you like to add?",
-    choices: ["Engineer", "Intern", "I don't want to add any more team members"]
-  }
-).then(({ member }) => {
-  if (member === "Engineer"){
-    this.promptEngineer();
-  } else if (member === "Intern"){
-    this.promptEngineer();
-  }  else {
-    return;
-    // return this.generateHTML;
-  }
-});
+  console.log("Team question!");
+// inquirer.prompt(
+//   {
+//     type: "list",
+//     name: "member",
+//     message: "Which type of team member would you like to add?",
+//     choices: ["Engineer", "Intern", "I don't want to add any more team members"]
+//   }
+// ).then(({ member }) => {
+//   if (member === "Engineer"){
+//     this.promptEngineer();
+//   } else if (member === "Intern"){
+//     this.promptEngineer();
+//   }  else {
+//     return;
+//     // return this.generateHTML;
+//   }
+// });
   }
 
-buildTeam.prototype.promptEngineer = function(){
-  inquirer.prompt (
-    {
-      type: "input",
-      name: "name",
-      message: "What is the engineer's name?",
-      validate: nameInput => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log("Please enter the engineer's name!");
-            return false;
-          }
-   }
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "What is the engineer's id?",
-    validate: idInput => {
-        if (idInput) {
-          return true;
-        } else {
-          console.log("Please enter the team manager's id number!");
-          return false;
-        }
-  }
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What is the engineer's email?",
-    validate: emailInput => {
-        if (emailInput) {
-          return true;
-        } else {
-          console.log("Please enter the engineer's email address!");
-          return false;
-        }
-  }
-  },
-  {
-    type: "input",
-    name: "github",
-    message: "What is the engineer's GitHub username?",
-    validate: githubInput => {
-        if (githubInput) {
-          return true;
-        } else {
-          console.log("Please enter the engineer's GitHub username!");
-          return false;
-        }
-  }
-  }
-  )
-  .then(
-    ({name, id, email, github}) => {
-      this.team.push(new Engineer(name, id, email, github));
-      this.promptTeamMember();
-    }
-  )
-};
+// buildTeam.prototype.promptEngineer = function(){
+  // inquirer.prompt (
+  //   {
+  //     type: "input",
+  //     name: "name",
+  //     message: "What is the engineer's name?",
+  //     validate: nameInput => {
+  //         if (nameInput) {
+  //           return true;
+  //         } else {
+  //           console.log("Please enter the engineer's name!");
+  //           return false;
+  //         }
+  //  }
+  // },
+  // {
+  //   type: "input",
+  //   name: "id",
+  //   message: "What is the engineer's id?",
+  //   validate: idInput => {
+  //       if (idInput) {
+  //         return true;
+  //       } else {
+  //         console.log("Please enter the team manager's id number!");
+  //         return false;
+  //       }
+  // }
+  // },
+  // {
+  //   type: "input",
+  //   name: "email",
+  //   message: "What is the engineer's email?",
+  //   validate: emailInput => {
+  //       if (emailInput) {
+  //         return true;
+  //       } else {
+  //         console.log("Please enter the engineer's email address!");
+  //         return false;
+  //       }
+  // }
+  // },
+  // {
+  //   type: "input",
+  //   name: "github",
+  //   message: "What is the engineer's GitHub username?",
+  //   validate: githubInput => {
+  //       if (githubInput) {
+  //         return true;
+  //       } else {
+  //         console.log("Please enter the engineer's GitHub username!");
+  //         return false;
+  //       }
+  // }
+  // }
+  // )
+  // .then(
+  //   ({name, id, email, github}) => {
+  //     this.team.push(new Engineer(name, id, email, github));
+  //     this.promptTeamMember();
+  //   }
+  // )
+// };
 
-buildTeam.prototype.promptIntern = function() {
-  inquirer.prompt (
-    {
-      type: "input",
-      name: "name",
-      message: "What is the intern's name?",
-      validate: nameInput => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log("Please enter the intern's name!");
-            return false;
-          }
-   }
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "What is the intern's id?",
-    validate: idInput => {
-        if (idInput) {
-          return true;
-        } else {
-          console.log("Please enter the intern's id number!");
-          return false;
-        }
-  }
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What is the intern's email?",
-    validate: emailInput => {
-        if (emailInput) {
-          return true;
-        } else {
-          console.log("Please enter the intern's email address!");
-          return false;
-        }
-  }
-  },
-  {
-    type: "input",
-    name: "school",
-    message: "What is the intern's school?",
-    validate: schoolInput => {
-        if (schoolInput) {
-          return true;
-        } else {
-          console.log("Please enter the intern's school!");
-          return false;
-        }
-  }
-  }
-  )
-  .then(
-    ({name, id, email, school}) => {
-      this.team.push(new Intern(name, id, email, school));
-      this.promptTeamMember();
-    });
-}
+// buildTeam.prototype.promptIntern = function() {
+//   inquirer.prompt (
+//     {
+//       type: "input",
+//       name: "name",
+//       message: "What is the intern's name?",
+//       validate: nameInput => {
+//           if (nameInput) {
+//             return true;
+//           } else {
+//             console.log("Please enter the intern's name!");
+//             return false;
+//           }
+//    }
+//   },
+//   {
+//     type: "input",
+//     name: "id",
+//     message: "What is the intern's id?",
+//     validate: idInput => {
+//         if (idInput) {
+//           return true;
+//         } else {
+//           console.log("Please enter the intern's id number!");
+//           return false;
+//         }
+//   }
+//   },
+//   {
+//     type: "input",
+//     name: "email",
+//     message: "What is the intern's email?",
+//     validate: emailInput => {
+//         if (emailInput) {
+//           return true;
+//         } else {
+//           console.log("Please enter the intern's email address!");
+//           return false;
+//         }
+//   }
+//   },
+//   {
+//     type: "input",
+//     name: "school",
+//     message: "What is the intern's school?",
+//     validate: schoolInput => {
+//         if (schoolInput) {
+//           return true;
+//         } else {
+//           console.log("Please enter the intern's school!");
+//           return false;
+//         }
+//   }
+//   }
+//   )
+//   .then(
+//     ({name, id, email, school}) => {
+//       this.team.push(new Intern(name, id, email, school));
+//       this.promptTeamMember();
+//     });
+// }
 
-promptManager();
+new buildTeam().promptManager();
 
 
 
